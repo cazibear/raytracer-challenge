a74c2a5c32283b368895175f0591efb039be2ea2
Callum Carnegie <ccarnegie98@gmail.com>
2022-08-29 22:28:50 +0100

    creating a PPM file to show the trajectory

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,17 +1,23 @@
+import java.io.File
+import kotlin.math.roundToInt
+
 fun main(args: Array<String>) {
 	data class Projectile(var position: Tuple, var velocity: Tuple)
 	data class Environment(val gravity: Tuple, val wind: Tuple)
+	data class Position(val x: Double, val y: Double)
 
-	val p = Projectile(Point(0, 1, 0), Vector(1, 1, 0).normalize())
+	val p = Projectile(Point(0, 1, 0), Vector(1, 1.8, 0).normalize() * 11.25)
 	val e = Environment(Vector(0, -0.1, 0), Vector(-0.01, 0, 0))
+	val c = Canvas(900, 550)
 	var tick = 0
 
 	while (p.position.y >= 0) {
-		println("${p.position.x}\t${p.position.y}")
-
-		p.position += ( p.velocity)
-		p.velocity += ( e.gravity + e.wind)
+		c[p.position.x.roundToInt(), c.height - p.position.y.roundToInt()] = Colour(0, 1, 0)
+		p.position += (p.velocity)
+		p.velocity += (e.gravity + e.wind)
 
 		tick++
 	}
+
+	File("trajectory.ppm").writeText(c.toPPM())
 }
